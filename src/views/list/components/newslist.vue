0806d3763223686bdeabb250006510bab36207c4
梁海军 <1090631724@qq.com>
2024-04-14 22:28:46 +0800

    20240414夜晚提交

--- src/views/list/components/newslist.vue
+++ src/views/list/components/newslist.vue
@@ -2,6 +2,48 @@
     <div class="newslist">
 
 
+        <div v-for="item in newsList" class="newselem">
+            <div class="img">
+                <el-image :src="item.img" fit="contain" style="width:100%;height:100%;">
+                    <div slot="error" class="image-slot">
+                        <i class="el-icon-picture-outline"></i>
+                    </div>
+                </el-image>
+            </div>
+            <div class="flex">
+                <div class="newLeft">
+                    <div class="newLefttop">
+                        <div>{{ item.date.split('-')[2] }}</div>
+                        <div>
+                            <span>
+                                {{ item.date.split('-')[1] }}, </span><span> {{ item.date.split('-')[0] }}
+                            </span>
+                        </div>
+                    </div>
+                    <div class="icon-edit">
+                        <i class="el-icon-edit"></i>
+                    </div>
+                </div>
+                <div class="newRight">
+                    <div class="title">{{ item.title }}</div>
+                    <div class="flex publish">
+                        By
+                        <el-link type="warning" :underline="false">{{ item.author }}</el-link>
+                        | <el-link type="warning" :underline="false">{{ item.source }}</el-link>
+                    </div>
+                    <div>
+                        {{ item.content }}
+                    </div>
+                </div>
+            </div>
+
+            <div class="bottom-line flex">
+
+                <el-link type="warning" :underline="false"> Read More <i class="el-icon-arrow-right"></i></el-link>
+
+            </div>
+        </div>
+
 
 
 
@@ -12,11 +54,95 @@
 export default {
     name: 'newslist',
     props: {
-
+        newsList: {
+            type: Array,
+            default() {
+                return []
+            }
+        }
     }
 }
 </script>
 
 <style lang="less" scoped>
-.newslist {}
+.newslist {
+
+    .flex {
+        display: flex;
+        justify-content: flex-start;
+
+    }
+
+    .bottom-line {
+        border-bottom: 1px solid #e7e4e2;
+        text-align: right;
+        padding: 8px 0;
+        justify-content: flex-end;
+        align-items: center;
+    }
+
+    .newselem {
+        margin-bottom: 65px;
+
+        .title {
+            color: #f09a3e;
+            font-size: 24px;
+            font-weight: 600;
+        }
+
+        .newLeft {
+            div.newLefttop {
+                background: #f09a3e;
+                width: 80px;
+                height: 80px;
+                padding: 8px 0 5px;
+                color: #fff;
+                display: block;
+                text-align: center;
+
+                div:first-child {
+                    font-size: 29px;
+                    display: inline-block;
+                }
+
+                div:last-child {
+                    font-size: 15px;
+                }
+            }
+
+            div.icon-edit {
+                background: #e7e4e2;
+                width: 80px;
+                height: 60px;
+                padding: 8px 0 5px;
+                color: #fff;
+                display: block;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                margin-top: 5px;
+
+                i {
+                    font-size: 22px;
+                    color: #f09a3e;
+                }
+            }
+        }
+
+        .newRight {
+            flex: 1;
+            margin-left: 20px;
+
+            .publish {
+                margin: 10px 0;
+            }
+
+            .el-link {
+                margin: 0 6px;
+            }
+        }
+    }
+
+
+}
 </style>
--- src/views/zzrz/index.vue
+++ src/views/zzrz/index.vue
@@ -1,175 +1,213 @@
 <template>
-    <page-header-wrapper
-    >
-      <template v-slot:extraContent>
-        <!-- <div style="width: 155px; margin-top: -20px;"><img style="width: 100%" :src="extraImage" /></div> -->
-      </template>
-      <!-- <a-list
-        rowKey="id"
-        :grid="{gutter: 24, lg: 3, md: 2, sm: 1, xs: 1}"
-        :dataSource="dataSource"
-        class="card-list"
-      >
-        <a-list-item slot="renderItem" slot-scope="item"> -->
+  <page-header-wrapper :title="false">
+
+    <div class="zzrz">
+
+      <div class="imgcontent flex">
+        <div class="">
+
+          <div class="flex title">
+            <p>1. 参加培训</p>
+          </div>
+          <div class="flex subtitle">
+            <p>参与者需要参加由我们的急救培训课程</p>
+          </div>
+          <div class="bottomline">
+            <div class="bottomlinecenter"></div>
+          </div>
+          <div class="flex content">
+            <div>
+              这些课程通常涵盖心肺复苏（CPR）、人工呼吸、止血、骨折固定等基本急救技能的培训，以及应急处置流程、传染病防控等方面的知识培训。 </div>
+          </div>
+
+          <div class="flex">
+            <el-button type="warning">APP下载</el-button>
+          </div>
+          <div class="flex">
+            <!-- <img style="width: 100%;" src="@/assets/delive.png" alt=""> -->
+          </div>
+
+        </div>
+      </div>
+
+
+      <div class="imgcontent flex">
+        <div class="">
+          <div class="flex">
+            <img src="@/assets/step1.png" alt="">
+          </div>
+          <div class="flex title">
+            <p>2. 通过考核</p>
+          </div>
+          <div class="flex subtitle">
+            <p>在完成培训课程后，参与者需要通过相关的考核评估</p>
+          </div>
+          <div class="bottomline">
+            <div class="bottomlinecenter"></div>
+          </div>
+          <div class="flex content">
+            <div>
+              以证明他们掌握了必要的急救技能和知识。考核通常包括理论知识测试和实际操作技能考核。 </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="imgcontent flex">
+        <div class="">
+          <div class="flex">
+            <img src="@/assets/step2.png" alt="">
+          </div>
+          <div class="flex title">
+            <p>3. 获得认证证书</p>
+          </div>
+          <div class="flex subtitle">
+            <p>资质取得</p>
+          </div>
+          <div class="bottomline">
+            <div class="bottomlinecenter"></div>
+          </div>
+          <div class="flex content">
+            <div>
+              通过考核后，参与者将获得急救师认证证书，证明他们具备了相应的急救能力和资格。这些认证证书通常由专业机构或组织颁发，并具有一定的权威性和公信力。 </div>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+
+
+
+
+  </page-header-wrapper>
+</template>
   
-          <a-card :bordered="false">
-              <a-row :gutter="16">
-                <a-col class="gutter-row" v-for="(item, index) in teachingList" :span="6">
-                  <div data-v-373a1af0="" class="expert-box border-line">
-                    <div data-v-373a1af0="" class="">
-                      <!-- <img data-v-373a1af0="" :src="item.avatar" alt="" class="expert-img" /> -->
-                      <img width="150" :src="item.img" alt="" />
-                    </div>
-                    <div data-v-373a1af0="" class="">
-                      <div data-v-373a1af0="" class="expert-name">
-                        <!-- <img :src="item.img" alt="" /> -->
-                        <!-- {{ item.title }} -->
-                      </div>
-                      <p data-v-373a1af0="" class="expert-intro">
-                        {{ item.title }}
-                      </p>
-                    </div>
-                  </div>
-                </a-col>
-              </a-row>
-          </a-card>
-  
-        <!-- </a-list-item>
-      </a-list> -->
-    </page-header-wrapper>
-  </template>
-  
-  <script>
-  
-  const dataSource = []
-  dataSource.push({})
-  for (let i = 0; i < 11; i++) {
-    dataSource.push({
-      id: i,
-      title: 'Alipay',
-      avatar: 'https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png',
-      content: '在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。'
-    })
-  }
-  
-  export default {
-    name: 'CardList',
-    data () {
-      this.tabList = [
-        { key: 'tab1', tab: '快速开始' },
-        { key: 'tab2', tab: '产品简介' },
-        { key: 'tab3', tab: '产品文档' }
-      ]
-      return {
-        tabActiveKey: 'tab1',
-  
-        extraImage: 'https://gw.alipayobjects.com/zos/rmsportal/RzwpdLnhmvDJToTdfDPe.png',
-        dataSource,
-        teachingList: [
-          {
-            title: ' 《医学急救与防疫（小学低年级）》 ',
-            img: require('@/assets/textbook1.png'),
-          },
-          {
-            title: ' 《医学急救与防疫（小学高年级）》 ',
-            img: require('@/assets/textbook2.png'),
-          },
-          {
-            title: '  《医学急救与防疫（初中）》  ',
-            img: require('@/assets/textbook3.png'),
-          },
-          {
-            title: ' 《校园急救》  ',
-            img: require('@/assets/textbook4.png'),
-          },
-        ],
-      }
-    },
-    methods: {
-      testFun () {
-        this.$message.info('快速开始被点击！')
-      }
+<script>
+
+const dataSource = []
+dataSource.push({})
+for (let i = 0; i < 11; i++) {
+  dataSource.push({
+    id: i,
+    title: 'Alipay',
+    avatar: 'https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png',
+    content: '在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。'
+  })
+}
+
+export default {
+  name: 'CardList',
+  data() {
+    this.tabList = [
+      { key: 'tab1', tab: '快速开始' },
+      { key: 'tab2', tab: '产品简介' },
+      { key: 'tab3', tab: '产品文档' }
+    ]
+    return {
+      tabActiveKey: 'tab1',
+
+      extraImage: 'https://gw.alipayobjects.com/zos/rmsportal/RzwpdLnhmvDJToTdfDPe.png',
+      dataSource,
+      teachingList: [
+        {
+          title: ' 《医学急救与防疫（小学低年级）》 ',
+          img: require('@/assets/textbook1.png'),
+        },
+        {
+          title: ' 《医学急救与防疫（小学高年级）》 ',
+          img: require('@/assets/textbook2.png'),
+        },
+        {
+          title: '  《医学急救与防疫（初中）》  ',
+          img: require('@/assets/textbook3.png'),
+        },
+        {
+          title: ' 《校园急救》  ',
+          img: require('@/assets/textbook4.png'),
+        },
+      ],
+    }
+  },
+  methods: {
+    testFun() {
+      this.$message.info('快速开始被点击！')
     }
   }
-  </script>
+}
+</script>
   
-  <style lang="less" scoped>
-    @import "~@/components/index.less";
-  
-    .card-list {
-      :deep(.ant-card-body:hover) {
-        .ant-card-meta-title>a {
-          color: @primary-color;
-        }
-      }
-  
-      :deep(.ant-card-meta-title) {
-        margin-bottom: 12px;
-  
-        &>a {
-          display: inline-block;
-          max-width: 100%;
-          color: rgba(0,0,0,.85);
-        }
-      }
-  
-      :deep(.meta-content) {
-        position: relative;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        display: -webkit-box;
-        height: 64px;
-        -webkit-line-clamp: 3;
-        -webkit-box-orient: vertical;
-  
-        margin-bottom: 1em;
-      }
+<style lang="less" scoped>
+// @import "~@/components/index.less";
+
+
+.zzrz {
+
+  .imgcontent {
+    // width: 100%;
+    // height: 900px;
+    // background: url(@/assets/blog_post_header_top_bg_3.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    // margin: 30px;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    color: #FFF;
+    margin-bottom: 50px;
+
+    i {
+      font-size: 50px;
     }
-  
-    .card-avatar {
-      width: 48px;
-      height: 48px;
-      border-radius: 48px;
+
+    .flex {
+      text-align: center;
+      display: flex;
+      justify-content: center;
     }
-  
-    .ant-card-actions {
-      background: #f7f9fa;
-  
-      li {
-        float: left;
-        text-align: center;
-        margin: 12px 0;
-        color: rgba(0, 0, 0, 0.45);
-        width: 50%;
-  
-        &:not(:last-child) {
-          border-right: 1px solid #e8e8e8;
-        }
-  
-        a {
-          color: rgba(0, 0, 0, .45);
-          line-height: 22px;
-          display: inline-block;
-          width: 100%;
-          &:hover {
-            color: @primary-color;
-          }
-        }
+
+    .title {
+      font-size: 34px;
+      font-weight: 600;
+      color: #333c4e;
+    }
+
+    .subtitle {
+      font-size: 20px;
+      color: #7E8890;
+    }
+
+    .bottomline {
+      margin-bottom: 30px;
+      text-align: center;
+      display: flex;
+      justify-content: center;
+
+      .bottomlinecenter {
+        height: 1px;
+        width: 200px;
+        background: #f09a3e;
       }
     }
-  
-    .new-btn {
-      background-color: #fff;
-      border-radius: 2px;
-      width: 100%;
-      height: 188px;
+
+    .content {
+      font-size: 15px;
+      color: #7E8890;
+
     }
-  
-    .expert-box:hover {
-      -webkit-transform: scale(1.1);
-      transform: scale(1.1);
+
+    div {
+      >div {
+        margin-bottom: 20px;
+      }
+
+    }
+
   }
-  
-  
-  </style>
+
+  .imgcontent:first-child {
+    background: #FFF;
+  }
+}
+</style>
   
